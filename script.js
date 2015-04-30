b7e2d5ca33488f542446dea2322efa0f11d6fd87
Kyra Fillmore <kfillmore27@gmail.com>
2015-04-30 17:45:23 -0400

    Add hourly toggle functionality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var main = function() {
-  
+  $('.day').click(function(){
+  	$(this).next().toggle();
+ 		$(this).find('span').toggleClass('glyphicon-plus, glyphicon-minus');
+  });
 };
 
 $(document).ready(main);
